d21c032ecc69fa57f372e8d2c7acc2cb027c7531
Felix Sun <fs.felix.sun@hotmail.com>
2015-07-04 16:52:53 -0400

    finished redesign of page

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,11 +7,11 @@
 // var s_4_height = $(".season4").outerHeight() + 14;
 // var s_5_height = $(".season5").outerHeight() + 14;
 
-var s_1_height = $("#asc").children(".season1").outerHeight() + 14;
-var s_2_height = $("#asc").children(".season2").outerHeight() + 14;
-var s_3_height = $("#asc").children(".season3").outerHeight() + 14;
-var s_4_height = $("#asc").children(".season4").outerHeight() + 14;
-var s_5_height = $("#asc").children(".season5").outerHeight() + 14;
+var s_1_height = $("#asc").children(".season1").outerHeight();
+var s_2_height = $("#asc").children(".season2").outerHeight();
+var s_3_height = $("#asc").children(".season3").outerHeight();
+var s_4_height = $("#asc").children(".season4").outerHeight();
+var s_5_height = $("#asc").children(".season5").outerHeight();
 var a_or_d = true;
 
 console.log(s_1_height);
@@ -22,17 +22,17 @@ console.log(s_5_height);
 
 $( window ).resize(function() {
   if (a_or_d == true){
-    s_1_height = $("#asc").children(".season1").outerHeight() + 14;
-    s_2_height = $("#asc").children(".season2").outerHeight() + 14;
-    s_3_height = $("#asc").children(".season3").outerHeight() + 14;
-    s_4_height = $("#asc").children(".season4").outerHeight() + 14;
-    s_5_height = $("#asc").children(".season5").outerHeight() + 14;
+    s_1_height = $("#asc").children(".season1").outerHeight();
+    s_2_height = $("#asc").children(".season2").outerHeight();
+    s_3_height = $("#asc").children(".season3").outerHeight();
+    s_4_height = $("#asc").children(".season4").outerHeight();
+    s_5_height = $("#asc").children(".season5").outerHeight();
   }else{
-    s_1_height = $("#desc").children(".season5").outerHeight() + 14;
-    s_2_height = $("#desc").children(".season4").outerHeight() + 14;
-    s_3_height = $("#desc").children(".season3").outerHeight() + 14;
-    s_4_height = $("#desc").children(".season2").outerHeight() + 14;
-    s_5_height = $("#desc").children(".season1").outerHeight() + 14;
+    s_1_height = $("#desc").children(".season5").outerHeight();
+    s_2_height = $("#desc").children(".season4").outerHeight();
+    s_3_height = $("#desc").children(".season3").outerHeight();
+    s_4_height = $("#desc").children(".season2").outerHeight();
+    s_5_height = $("#desc").children(".season1").outerHeight();
   }
 });
 
@@ -54,21 +54,21 @@ $(".down_arrow").click(function(event)
     
     if (a_or_d == true ){
       setTimeout(function(){ 
-          s_1_height = $("#asc").children(".season1").outerHeight() + 14;
-          s_2_height = $("#asc").children(".season2").outerHeight() + 14;
-          s_3_height = $("#asc").children(".season3").outerHeight() + 14;
-          s_4_height = $("#asc").children(".season4").outerHeight() + 14;
-          s_5_height = $("#asc").children(".season5").outerHeight() + 14;
+          s_1_height = $("#asc").children(".season1").outerHeight();
+          s_2_height = $("#asc").children(".season2").outerHeight();
+          s_3_height = $("#asc").children(".season3").outerHeight();
+          s_4_height = $("#asc").children(".season4").outerHeight();
+          s_5_height = $("#asc").children(".season5").outerHeight();
           console.log(s_1_height);
       }, 502); 
     }
     else{
        setTimeout(function(){ 
-          s_1_height = $("#desc").children(".season5").outerHeight() + 14;
-          s_2_height = $("#desc").children(".season4").outerHeight() + 14;
-          s_3_height = $("#desc").children(".season3").outerHeight() + 14;
-          s_4_height = $("#desc").children(".season2").outerHeight() + 14;
-          s_5_height = $("#desc").children(".season1").outerHeight() + 14;
+          s_1_height = $("#desc").children(".season5").outerHeight();
+          s_2_height = $("#desc").children(".season4").outerHeight();
+          s_3_height = $("#desc").children(".season3").outerHeight();
+          s_4_height = $("#desc").children(".season2").outerHeight();
+          s_5_height = $("#desc").children(".season1").outerHeight();
       }, 502); 
     }
     
@@ -104,11 +104,11 @@ $(".toggle_circle").click(function(event){
       }, 501);
       //logic
 
-      s_1_height = $("#desc").children(".season5").outerHeight() + 14;
-      s_2_height = $("#desc").children(".season4").outerHeight() + 14;
-      s_3_height = $("#desc").children(".season3").outerHeight() + 14;
-      s_4_height = $("#desc").children(".season2").outerHeight() + 14;
-      s_5_height = $("#desc").children(".season1").outerHeight() + 14;
+      s_1_height = $("#desc").children(".season5").outerHeight();
+      s_2_height = $("#desc").children(".season4").outerHeight();
+      s_3_height = $("#desc").children(".season3").outerHeight();
+      s_4_height = $("#desc").children(".season2").outerHeight();
+      s_5_height = $("#desc").children(".season1").outerHeight();
       console.log(s_1_height);
       console.log(s_2_height);
       console.log(s_3_height);
@@ -135,11 +135,11 @@ $(".toggle_circle").click(function(event){
       }, 0); 
 
       // //logic
-      s_1_height = $("#asc").children(".season1").outerHeight() + 14;
-      s_2_height = $("#asc").children(".season2").outerHeight() + 14;
-      s_3_height = $("#asc").children(".season3").outerHeight() + 14;
-      s_4_height = $("#asc").children(".season4").outerHeight() + 14;
-      s_5_height = $("#asc").children(".season5").outerHeight() + 14;
+      s_1_height = $("#asc").children(".season1").outerHeight();
+      s_2_height = $("#asc").children(".season2").outerHeight();
+      s_3_height = $("#asc").children(".season3").outerHeight();
+      s_4_height = $("#asc").children(".season4").outerHeight();
+      s_5_height = $("#asc").children(".season5").outerHeight();
 
       console.log(s_1_height);
       console.log(s_2_height);
